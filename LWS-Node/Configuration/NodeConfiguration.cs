ba9429211e1b27c63ac7946cc76b2d9e6045175c
kangdroid <hyunwoo286@gmail.com>
2021-11-17 22:23:21 +0900

    NodeConfiguration: Save token on filesystem

--- LWS-Node/Configuration/NodeConfiguration.cs
+++ LWS-Node/Configuration/NodeConfiguration.cs
@@ -1,9 +1,14 @@
 using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace LWS_Node.Configuration
 {
     public class NodeConfiguration
     {
+        private const string TokenPath = "/tmp/lws_token";
+        
         public string NodeKey { get; set; }
         public string NodeNickName { get; set; }
         public int NodeMaximumCpu { get; set; }
@@ -11,7 +16,31 @@ namespace LWS_Node.Configuration
 
         public NodeConfiguration()
         {
-            NodeKey = Guid.NewGuid().ToString();
+            if (File.Exists(TokenPath))
+            {
+                using var fileStream = File.OpenRead(TokenPath);
+                using var fileReader = new StreamReader(fileStream);
+                NodeKey = fileReader.ReadLine() 
+                          ?? throw new NullReferenceException("Token file read from stream, but null returned");
+            }
+            else
+            {
+                NodeKey = GenerateRandomToken();
+                
+                using var fileStream = File.OpenWrite(TokenPath);
+                using var fileWriter = new StreamWriter(fileStream);
+                fileWriter.WriteLine(NodeKey);
+                fileWriter.Flush();
+            }
+        }
+
+        private string GenerateRandomToken(int length = 64)
+        {
+            var random = new Random();
+            var charDictionary = "1234567890=#_+abcdefghijklmnopqrstuvwxyz";
+
+            return new string(Enumerable.Repeat(charDictionary, length)
+                .Select(a => a[random.Next(charDictionary.Length)]).ToArray());
         }
     }
 }
